71d18710d537ae0c992f0f781cf1fd58a5ac7d23
aditya.mmy <devnull@localhost>
2011-10-30 01:47:27 +0530

    Committing initial job tracking design

--- pyaxel.py
+++ pyaxel.py
@@ -10,6 +10,15 @@ import urllib2
 
 from optparse import OptionParser
 
+
+"""
+1) Get url, determine file size, dest file name.
+2) Get each chunk size
+"""
+
+BLK_SIZE = 1024*1024
+MAX_RETRIES = 10
+
 std_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; '
         'en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6',
@@ -19,130 +28,101 @@ std_headers = {
     'Accept-Language': 'en-us,en;q=0.5',
 }
 
-
-class ConnectionState:
-    def __init__(self, n_conn, filesize):
+class DownloadState:
+    def __init__(self, n_conn, url, filesize, filename):
         self.n_conn = n_conn
         self.filesize = filesize
-        self.progress = [0 for i in range(n_conn)]
+        self.filename = filename
+        self.url = url
+        self.ongoing_jobs = {}
         self.elapsed_time = 0
-        self.chunks = [(filesize / n_conn) for i in range(n_conn)]
-        self.chunks[0] += filesize % n_conn
 
-    def download_sofar(self):
-        dwnld_sofar = 0
-        for rec in self.progress:
-            dwnld_sofar += rec
-        return dwnld_sofar
+    def write_state(self):
+        pass
 
-    def update_time_taken(self, elapsed_time):
-        self.elapsed_time += elapsed_time
+class JobTracker:
+    def __init__(self, download_state):
+        self.download_state = download_state
+        filesize = download_state.filesize
+        self.job_count = (filesize + BLK_SIZE - 1) / BLK_SIZE
+        self.next_todo_job = 0
+        self.lock = threading.Lock()
 
-    def update_data_downloaded(self, fetch_size, conn_id):
-        self.progress[conn_id] += fetch_size
+    def get_next_job(self):
+        self.lock.acquire()
+        r = self.next_todo_job
+        if r >= self.job_count:
+            r = None
+        else:
+            self.next_todo_job += 1
+        self.lock.release()
+        return r
+        
+class Worker(threading.Thread):
+    def __init__(self, name, download_state, job_tracker):
+        threading.Thread.__init__(self)
+        self.name = str(name)
+        self.download_state = download_state
+        self.job_tracker = job_tracker
+        self.offset = 0
+        self.length = 0
+        self._need_to_quit = False
 
-    def resume_state(self, in_fd):
+    def __update_offset_and_length(self, jobNo):
+        self.offset = jobNo * BLK_SIZE
+        self.length = min(BLK_SIZE, self.download_state.filesize - self.offset)
+
+    def __open_output_file(self):
         try:
-            saved_obj = cPickle.load(in_fd)
-        except cPickle.UnpicklingError:
-            print "State file is corrupted"
-            #now start download from the beginning
-            return 
+            out_fd = os.open(self.download_state.filename + ".part",
+                             os.O_CREAT | os.O_WRONLY)
+            os.lseek(out_fd, self.offset, os.SEEK_SET)
+            return out_fd
+        except OSError, e:
+            # TODO: stop all threads and exit
+            print e.message
 
-        self.n_conn = saved_obj.n_conn
-        self.filesize = saved_obj.filesize
-        self.progress = saved_obj.progress
-        self.chunks = saved_obj.chunks
-        self.elapsed_time = saved_obj.elapsed_time
+    def __get_remote_data(self):
+        request = urllib2.Request(self.download_state.url, None, std_headers)
+        request.add_header('Range', 'bytes=%d-%d' % (self.offset,
+                                                     self.offset + self.length - 1))
+        attempts = 0
+        remote_file = None
+        data_block = None
+        while attempts < MAX_RETRIES:
+            try:
+                remote_file = urllib2.urlopen(request)
+                data_block = remote_file.read()
+                if len(data_block) == self.length:
+                    #print "Got bytes %d to %d" % \
+                    #    (self.offset, self.offset + self.length - 1)
+                    break
+            except urllib2.URLError, u:
+                print "Failed to open url with error: %s. " + \
+                    "Retrying connection %s" % (u.message, self.name)
+            attempts += 1
 
-    def save_state(self, out_fd):
-        #out_fd will be closed after save_state() is completed
-        #to ensure that state is written onto the disk
-        cPickle.dump(self, out_fd)
+        if len(data_block) != self.length:
+            print "Could not get data from offset %d to %d" \
+                % (self.offset, self.offset + self.length)
+            # TODO raise exception or exit
+        return data_block
 
-
-class ProgressBar:
-    def __init__(self, n_conn, conn_state):
-        self.n_conn = n_conn
-        self.dots = ["" for i in range(n_conn)]
-        self.conn_state = conn_state
-
-    def _get_term_width(self):
-        term_rows, term_cols = map(int, os.popen('stty size', \
-                                                     'r').read().split())
-        return term_cols
-
-    def _get_download_rate(self, bytes):
-        ret_str = report_bytes(bytes)
-        ret_str += "/s."
-        return len(ret_str), ret_str
-
-    def _get_percentage_complete(self, dl_len):
-        assert self.conn_state.filesize != 0
-        ret_str = str(dl_len * 100 / self.conn_state.filesize) + "%."
-        return len(ret_str), ret_str
-
-    def _get_time_left(self, time_in_secs):
-        ret_str = ""
-        mult_list = [60, 60 * 60, 60 * 60 * 24]
-        unit_list = ["second(s)", "minute(s)", "hour(s)", "day(s)"]
-        for i in range(len(mult_list)):
-            if time_in_secs < mult_list[i]:
-                pval = int(time_in_secs / (mult_list[i - 1] if i > 0 else 1))
-                ret_str = "%d %s" % (pval, unit_list[i])
-                break
-        if len(ret_str) == 0:
-            ret_str = "%d %s." % (int(time_in_secs / mult_list[2]), \
-                                      unit_list[3])
-        return len(ret_str), ret_str
-
-    def _get_pbar(self, width):
-        ret_str = "["
-        for i in range(self.n_conn):
-            dots_list = ['=' for j in range((self.conn_state.progress[i] *
-                                             width) /
-                                            self.conn_state.chunks[i])]
-            self.dots[i] = "".join(dots_list)
-            if ret_str == "[":
-                ret_str += self.dots[i]
-            else:
-                ret_str += "|" + self.dots[i]
-            if len(self.dots[i]) < width:
-                ret_str += '>'
-                ret_str += "".join([' ' for i in range(width -
-                                                       len(self.dots[i]) - 1)])
-
-        ret_str += "]"
-        return len(ret_str), ret_str
-
-    def display_progress(self):
-        dl_len = 0
-        for rec in self.conn_state.progress:
-            dl_len += rec
-
-        assert(self.conn_state.elapsed_time > 0)
-        avg_speed = dl_len / self.conn_state.elapsed_time
-
-        ldr, drate = self._get_download_rate(avg_speed)
-        lpc, pcomp = self._get_percentage_complete(dl_len)
-        ltl, tleft = self._get_time_left((self.conn_state.filesize - dl_len) /
-                                         avg_speed if avg_speed > 0 else 0)
-        # term_width - #(|) + #([) + #(]) + #(strings) +
-        # 6 (for spaces and periods)
-        available_width = self._get_term_width() - (ldr + lpc +
-                                                    ltl) - self.n_conn - 1 - 6
-        lpb, pbar = self._get_pbar(available_width / self.n_conn)
-        sys.stdout.flush()
-        print "\r%s %s %s %s" % (drate, pcomp, tleft, pbar),
-
-
-def report_bytes(bytes):
-    if bytes == 0:
-        return "0b"
-    k = math.log(bytes, 1024)
-    ret_str = "%.2f%s" % (bytes / (1024.0 ** int(k)), "bKMGTPEY"[int(k)])
-    return ret_str
-
+    def run(self):
+        jobNo = self.job_tracker.get_next_job()
+        while jobNo != None:
+            self.__update_offset_and_length(jobNo)
+            if self._need_to_quit: return
+            output_file = self.__open_output_file()
+            if self._need_to_quit: return
+            self.download_state.ongoing_jobs[jobNo] = None
+            remote_block = self.__get_remote_data()
+            os.write(output_file, remote_block)
+            os.close(output_file)
+            del self.download_state.ongoing_jobs[jobNo]
+            self.download_state.write_state()
+            if self._need_to_quit: return
+            jobNo = self.job_tracker.get_next_job()
 
 def get_file_size(url):
     request = urllib2.Request(url, None, std_headers)
@@ -151,168 +131,44 @@ def get_file_size(url):
     # print content_length
     return int(content_length)
 
-
-class FetchData(threading.Thread):
-
-    def __init__(self, name, url, out_file, state_file,
-                 start_offset, conn_state):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.url = url
-        self.out_file = out_file
-        self.state_file = state_file
-        self.start_offset = start_offset
-        self.conn_state = conn_state
-        self.length = conn_state.chunks[name] - conn_state.progress[name]
-        self.sleep_timer = 0
-        self.need_to_quit = False
-        self.need_to_sleep = False
-
-    def run(self):
-        # Ready the url object
-        # print "Running thread with %d-%d" % (self.start_offset, self.length)
-        request = urllib2.Request(self.url, None, std_headers)
-        if self.length == 0:
-            return
-        request.add_header('Range', 'bytes=%d-%d' % (self.start_offset,
-                                                     self.start_offset + \
-                                                     self.length))
-        while 1:
-            try:
-                data = urllib2.urlopen(request)
-            except urllib2.URLError, u:
-                print "Connection", self.name, " did not start with", u
-            else:
-                break
-
-        # Open the output file
-        out_fd = os.open(self.out_file+".part", os.O_WRONLY)
-        os.lseek(out_fd, self.start_offset, os.SEEK_SET)
-
-        block_size = 1024
-        #indicates if connection timed out on a try
-        while self.length > 0:
-            if self.need_to_quit:
-                return
-
-            if self.need_to_sleep:
-                time.sleep(self.sleep_timer)
-                self.need_to_sleep = False
-
-            if self.length >= block_size:
-                fetch_size = block_size
-            else:
-                fetch_size = self.length
-            try:
-                data_block = data.read(fetch_size)
-                if len(data_block) == 0:
-                    print "Connection %s: [TESTING]: 0 sized block" + \
-                        " fetched." % (self.name)
-                if len(data_block) != fetch_size:
-                    print "Connection %s: len(data_block) != fetch_size" + \
-                        ", but continuing anyway." % (self.name)
-                    self.run()
-                    return
-
-            except socket.timeout, s:
-                print "Connection", self.name, "timed out with", s
-                self.run()
-                return
-
-            else:
-                retry = 0
-
-            #assert(len(data_block) == fetch_size)
-            self.length -= fetch_size
-            self.conn_state.update_data_downloaded(fetch_size, int(self.name))
-            os.write(out_fd, data_block)
-            self.start_offset += len(data_block)
-            #saving state after each blk of dwnld
-            state_fd = file(self.state_file, "wb")
-            self.conn_state.save_state(state_fd)
-            state_fd.close()
-
-def general_configuration():
-    # General configuration
-    urllib2.install_opener(urllib2.build_opener(urllib2.ProxyHandler()))
-    urllib2.install_opener(urllib2.build_opener(
-            urllib2.HTTPCookieProcessor()))
-    socket.setdefaulttimeout(120)         # 2 minutes
-
 def download(url, options):
     fetch_threads = []
     try:
-        output_file = url.rsplit("/", 1)[1]   # basename of the url
-
-        if options.output_file != None:
+        if not options.output_file:
+            output_file = url.rsplit("/", 1)[1]   # basename of the url
+        else:
             output_file = options.output_file
 
-        if output_file == "":
-            print "Invalid URL"
+        if not output_file:
+            print "Invalid URL or could not determine destination file name."
             sys.exit(1)
 
         print "Destination = ", output_file
 
         filesize = get_file_size(url)
+        print "Need to fetch %d bytes" % filesize
 
-        conn_state = ConnectionState(options.num_connections, filesize)
-        pbar = ProgressBar(options.num_connections, conn_state)
+        download_state = DownloadState(options.num_connections, url,
+                                       filesize, output_file)
+        job_tracker = JobTracker(download_state)
 
-        # Checking if we have a partial download available and resume
-        state_file = output_file + ".st"
-        try:
-            os.stat(state_file)
-        except OSError, o:
-            #statefile is missing for all practical purposes
-            pass
-        else:
-            state_fd = file(state_file, "r")
-            conn_state.resume_state(state_fd)
-            state_fd.close()
-
-        print "Need to fetch %s\n" % report_bytes(conn_state.filesize - sum(conn_state.progress))
-        #create output file with a .part extension to indicate partial download
-        out_fd = os.open(output_file+".part", os.O_CREAT | os.O_WRONLY)
-
-        start_offset = 0
-        start_time = time.time()
         for i in range(options.num_connections):
-            # each iteration should spawn a thread.
-            # print start_offset, len_list[i]
-            current_thread = FetchData(i, url, output_file, state_file,
-                                       start_offset + conn_state.progress[i],
-                                       conn_state)
+            current_thread = Worker(i, download_state, job_tracker)
             fetch_threads.append(current_thread)
             current_thread.start()
-            start_offset += conn_state.chunks[i]
 
         while threading.active_count() > 1:
-            #print "\n",progress
-            end_time = time.time()
-            conn_state.update_time_taken(end_time - start_time)
-            start_time = end_time
-            dwnld_sofar = conn_state.download_sofar()
-            if options.max_speed != None and \
-                    (dwnld_sofar / conn_state.elapsed_time) > \
-                    (options.max_speed * 1024):
-                for th in fetch_threads:
-                    th.need_to_sleep = True
-                    th.sleep_timer = dwnld_sofar / (options.max_speed * \
-                        1024 - conn_state.elapsed_time)
-
-            pbar.display_progress()
             time.sleep(1)
 
-        pbar.display_progress()
-
-        # at this point we are sure dwnld completed and can delete the
-        # state file and move the dwnld to output file from .part file
-        os.remove(state_file)
-        os.rename(output_file+".part", output_file)
+        # # at this point we are sure dwnld completed and can delete the
+        # # state file and move the dwnld to output file from .part file
+        # os.remove(state_file)
+        os.rename(output_file + ".part", output_file)
 
     except KeyboardInterrupt, k:
+        print "KeyboardInterrupt! Quitting."
         for thread in fetch_threads:
-            thread.need_to_quit = True
+            thread._need_to_quit = True
 
     except Exception, e:
         # TODO: handle other types of errors too.
@@ -320,9 +176,17 @@ def download(url, options):
         for thread in fetch_threads:
             thread._need_to_quit = True
 
+
+def urllib_conf():
+    # General configuration
+    urllib2.install_opener(urllib2.build_opener(urllib2.ProxyHandler()))
+    urllib2.install_opener(urllib2.build_opener(
+            urllib2.HTTPCookieProcessor()))
+    socket.setdefaulttimeout(120)         # 2 minutes
+
 def main(options, args):
     try:
-        general_configuration()
+        urllib_conf()
         url = args[0]
         download(url, options)
 
@@ -332,7 +196,6 @@ def main(options, args):
     except Exception, e:
         # TODO: handle other types of errors too.
         print e
-        pass
 
 if __name__ == "__main__":
 
@@ -348,8 +211,8 @@ if __name__ == "__main__":
                       help="don't print status messages to stdout")
     parser.add_option("-n", "--num-connections", dest="num_connections",
                       type="int", default=4,
-                      help="You can specify an alternative number of"
-                      " connections here.",
+                      help="You can specify the number of"
+                      " connections here. Default is 4.",
                       metavar="NUM")
     parser.add_option("-o", "--output", dest="output_file",
                       help="By default, data does to a local file of "
